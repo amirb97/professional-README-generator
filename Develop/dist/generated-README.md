e1c519c8f4cae7c032957028a9533a4bdafa5dd1
amirb97 <amirb2467@gmail.com>
2021-12-27 16:51:36 -0800

    functions implemented for the license badges, section, and links

--- Develop/dist/generated-README.md
+++ Develop/dist/generated-README.md
@@ -1,3 +1,18 @@
-
+# sadf
   ![APACHE License](https://img.shields.io/aur/license/android-studio.svg?)
-  
+  ## Description
+
+  dsafsafd
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [License](#license)
+    
+  ## License
+    
+  Click here for information on the [APACHE License](https://www.apache.org/licenses/LICENSE-2.0)
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if(license) {
@@ -17,32 +17,56 @@ function renderLicenseBadge(license) {
   }
 };
 
-// TODO: Create a function that returns the license link
+// Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if(license) {
-
+    for(let i=0; i<license.length; i++){
+      if(license[i] === 'Apache')
+      {
+        return `Click here for information on the [APACHE License](https://www.apache.org/licenses/LICENSE-2.0)`;
+      } else if (license[i] === 'GNU GPLv3') {
+        return `Click here for information on the [GPLv3 License](https://choosealicense.com/licenses/gpl-3.0/)`;
+      } else {
+        return `Click here for information on the [MIT License](https://choosealicense.com/licenses/mit/)`;
+      };
+    }
   } else {
     return '';
   }
 };
 
-// TODO: Create a function that returns the license section of README
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if(license) {
-
+    return `* [License](#license)
+    
+  ## License
+    
+  ${renderLicenseLink(license)}`;
   } else {
     return '';
   }
 };
 
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(data) {
   console.log(data);
-  return `
+  return `# ${data.title}
   ${renderLicenseBadge(data.license)}
-  `;
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ${renderLicenseSection(data.license)}`;
 }
 
 module.exports = generateMarkdown;
